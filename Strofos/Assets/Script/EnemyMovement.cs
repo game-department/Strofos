41d3e5e688da2fc04c88f3452ab1592de2a12ec9
Ivan Lianto <ivanlianto59@gmail.com>
2019-05-06 00:40:10 +0700

    Update Enemy Mmovement

--- Strofos/Assets/Script/EnemyMovement.cs
+++ Strofos/Assets/Script/EnemyMovement.cs
@@ -16,16 +16,27 @@ public class EnemyMovement : MonoBehaviour
 
     public Vector2 vector;
 
+    private SpriteRenderer image;
+
+    private void Start()
+    {
+        image = GetComponent<SpriteRenderer>();
+        targetA.x = A.transform.position.x;
+        targetB.x = B.transform.position.x;
+    }
+
     void Update()
     {
 
         float step = speed * Time.deltaTime;
         if(transform.position.x == targetB.x)
         {
+            image.flipX = true;
             vector = A.transform.position;
         }
         else if(transform.position.x == targetA.x)
         {
+            image.flipX = false;
             vector = B.transform.position;
         }
         transform.position = Vector2.MoveTowards(transform.position, vector, step);
